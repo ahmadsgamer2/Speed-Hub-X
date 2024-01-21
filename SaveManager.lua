b2e3ec6faa52ac49b332ff0768f1f32d91a0b5ae
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-01-21 19:15:16 +0400

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService("HttpService")
 
 local SaveManager = {} do
-	SaveManager.Folder = "FluentThunderZ"
+	SaveManager.Folder = "Fluent_SpeedHubX"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
