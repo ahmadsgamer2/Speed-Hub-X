42f0322e0b863dd3364d1475e551e131ffb25e17
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-01-28 17:54:12 +0400

    Update GetTheme.lua

--- SpeedHubLib_V2/GetTheme.lua
+++ SpeedHubLib_V2/GetTheme.lua
@@ -2,10 +2,10 @@ local Themes = {
   ["Default"] = {
     ["Color Hub 1"] = Color3.fromRGB(18, 18, 18),
     ["Color Hub 2"] = Color3.fromRGB(25, 25, 25),
-    ["Color Theme"] = Color3.fromRGB(30, 150, 240),
+    ["Color Theme"] = Color3.fromRGB(247, 22, 22),
     ["Color Text"] = Color3.fromRGB(240, 240, 240),
     ["Color Dark Text"] = Color3.fromRGB(200, 200, 200),
-    ["Color Stroke"] = Color3.fromRGB(80, 80, 80),
+    ["Color Stroke"] = Color3.fromRGB(247, 22, 22),
     ["Corner Radius"] = UDim.new(0, 5),
     ["Font"] = {
       Enum.Font.GothamBlack,
