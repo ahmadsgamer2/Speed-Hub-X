266d34550c4e661721389449f33657202bb789b6
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-02-08 16:32:02 +0400

    Update English.lua

--- Translate/English.lua
+++ Translate/English.lua
@@ -83,6 +83,7 @@ AutoSwanGlassesText = "Auto Swan Glasses"
 AutoSwanGlassesHopText = "Auto Swan Glasses [Hop]"
 -- Sea Rough
 SeaRoughText = "Sea Rough"
+AutoSailBoatText = "Auto Sail Boat"
 AutoFindLeviathanText = "Auto Find Leviathan"
 AutoKillEventText = "Auto Kill [Event]"
 AutoKillTerrorsharkText = "Auto Kill Terrorshark"
